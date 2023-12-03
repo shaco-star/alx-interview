bbe9dc275dc9f051270d06a854d182a89ccd0841
shaco-star <sherifm846@gmail.com>
2023-12-03 11:13:33 -0800

    commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        row += [last_row[j] + last_row[j+1] for j in range(len(last_row) - 1)]
+        row.append(1)
+        triangle.append(row)
+    return triangle
