15db20e498d54a54ea4938b97a477a5be098f67d
shaco-star <sherifm846@gmail.com>
2024-02-07 22:44:17 -0800

    fix-style

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,16 +4,14 @@ def island_perimeter(grid):
 
     for r in range(rows):
         for c in range(cols):
-            if grid[r][c] == 1:  # For each land cell
-                if r == 0 or grid[r-1][c] == 0:  # Check the cell above
+            if grid[r][c] == 1:
+                if r == 0 or grid[r-1][c] == 0:
                     perimeter += 1
-                if r == rows-1 or grid[r+1][c] == 0:  # Check the cell below
+                if r == rows-1 or grid[r+1][c] == 0:
                     perimeter += 1
-                if c == 0 or grid[r][c-1] == 0:  # Check the cell to the left
+                if c == 0 or grid[r][c-1] == 0:
                     perimeter += 1
-                if c == cols-1 or grid[r][c+1] == 0:  # Check the cell to the right
+                if c == cols-1 or grid[r][c+1] == 0:
                     perimeter += 1
 
     return perimeter
-
-
